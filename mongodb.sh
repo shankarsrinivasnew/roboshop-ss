8f652d3ada0812b589114a7f49bac53aa889a5ea
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-19 23:15:21 +0530

    sadf

--- mongodb.sh
+++ mongodb.sh
@@ -3,5 +3,5 @@ cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
