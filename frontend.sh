7eaa84f8e3995f227090013b801df2fcb271fa45
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-19 20:34:36 +0530

    added config

--- frontend.sh
+++ frontend.sh
@@ -5,4 +5,5 @@ rm -rvf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+cp configs/roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx 
