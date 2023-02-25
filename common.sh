d44500b0d83ae2a1d55fb27ea649e68207650267
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-25 22:23:45 +0530

    dgh

--- common.sh
+++ common.sh
@@ -25,6 +25,69 @@ print_header "installing package"
 yum install nodejs -y &>>${log_file}
 status_check $?
 
+app_user_setup
+
+print_header "installing new code"
+npm install &>>${log_file}
+status_check $?
+
+print_header "copying old code"
+cp ${code_dir}/configs/$component.service /etc/systemd/system/$component.service &>>${log_file}
+status_check $?
+
+print_header "restarting service"
+systemctl daemon-reload &>>${log_file}
+systemctl enable $component &>>${log_file}
+systemctl restart $component &>>${log_file}
+status_check $?
+
+schema_setup
+
+}
+
+java (){
+print_header "Installing maven "
+yum install maven -y  &>>${log_file}
+status_check $?
+
+app_user_setup
+
+print_header "installing dependencies"
+mvn clean package   &>>${log_file}
+mv target/$component-1.0.jar $component.jar  &>>${log_file}
+status_check $?
+
+schema_setup
+systemd_setup
+
+}
+
+schema_setup () {
+
+if [ "${schema_type}" == "mongo" ]; then
+
+print_header "loading mogo repositories"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
+
+print_header "loading schema"
+mongo --host mongodb.sstech.store </app/schema/$component.js &>>${log_file}
+status_check $?
+
+elif [ "${schema_type}" == "mysql" ]; then
+
+print_header "installing mysql"
+yum install mysql -y &>>${log_file}
+status_check $?
+
+print_header "loading mysql schema"
+mysql -h mysql.sstech.store -uroot -p${mysql_root_password} < /app/schema/$component.sql &>>${log_file}
+status_check $?
+
+}
+
+app_user_setup () {
 print_header "adding user"
 id roboshop &>>${log_file}
 if [ $? -ne 0 ]; then
@@ -46,46 +109,21 @@ print_header "downloading new code"
 curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>${log_file}
 status_check $?
 
-print_header "creating new directory"
-cd /app
-status_check $?
-
 print_header "unzipping new code"
 unzip /tmp/$component.zip &>>${log_file}
 cd /app
 status_check $?
-
-print_header "installing new code"
-npm install &>>${log_file}
-status_check $?
-
-print_header "copying old code"
-cp ${code_dir}/configs/$component.service /etc/systemd/system/$component.service &>>${log_file}
-status_check $?
-
-print_header "restarting service"
-systemctl daemon-reload &>>${log_file}
-systemctl enable $component &>>${log_file}
-systemctl restart $component &>>${log_file}
-status_check $?
-
-schema_setup
-
 }
 
-schema_setup () {
-
-if [ "${schema_type}" == "mongo" ]; then
-
-print_header "loading mogo repositories"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
-yum install mongodb-org-shell -y &>>${log_file}
+systemd_setup () {
+print_header "adding systemd file"
+cp {code_dir}/configs/$component.service /etc/systemd/system/$component.service  &>>${log_file}
 status_check $?
 
-print_header "loading schema"
-mongo --host mongodb.sstech.store </app/schema/$component.js &>>${log_file}
+print_header "starting systemd service of app"
+systemctl daemon-reload  &>>${log_file}
+systemctl enable $component  &>>${log_file}
+systemctl restart $component  &>>${log_file}
 status_check $?
-
-fi
 
 }
---
+++ shipping.sh
@@ -0,0 +1,15 @@
+source common.sh
+mysql_root_password=$1
+
+if [ -z "${mysql_root_password}"]; then
+echo -e "\e[31Misiing mysqld password\e[0m"
+exit 1
+fi
+
+component=shipping
+schema_type="mysql"
+java
+
+
+
+
