b1f826c345a1ca969e23b8ca3b60f29f71db3bbb
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-25 22:44:39 +0530

    sadf

--- shipping.sh
+++ shipping.sh
@@ -2,7 +2,7 @@ source common.sh
 mysql_root_password=$1
 
 if [ -z "${mysql_root_password}"]; then
-echo -e "\e[31Misiing mysqld password\e[0m"
+echo -e "\e[31mMisiing mysqld password\e[0m"
 exit 1
 fi
 
