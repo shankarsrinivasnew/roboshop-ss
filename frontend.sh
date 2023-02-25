a15c9eb9c22df11ab9c783f200227a1d1b5da6e9
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-25 18:47:21 +0530

    dsfgh

--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,22 @@
 code_dir=$(pwd)
 tmp_dir=/tmp/roboshop.log
 
-echo -e "\e[36m Installing nginx \e[0m"
+print_header () {
+    echo -e "\e[36m$1\e[0m "
+}
+
+print_header "Installing nginx"
 yum install nginx -y &>>/tmp/roboshop.log
-echo -e " \e[36m enabling systemd service \e[0m "
+print_header "enabling service"
 systemctl enable nginx &>>/tmp/roboshop.log
 systemctl start nginx &>>/tmp/roboshop.log
-echo -e " \e[36m removing old code \e[0m "
+print_header "Removing old code"
 rm -rvf /usr/share/nginx/html/* &>>/tmp/roboshop.log
-echo -e " \e[36m Downloading new code \e[0m "
+print_header "downloading new code"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
-echo -e " \e[36m copying code \e[0m "
+print_header "copying new code"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip  &>>/tmp/roboshop.log
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
-echo -e " \e[36m restarting service service \e[0m "
+print_header "restarting nginx"
 systemctl restart nginx &>>/tmp/roboshop.log
