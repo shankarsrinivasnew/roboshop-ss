0f83d69887fb2a71070681bc871824eb87bb6e90
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-25 21:50:16 +0530

    sdf

--- mysql.sh
+++ mysql.sh
@@ -19,11 +19,10 @@ status_check $?
 
 mysql_root_password=$1
 
+print_header "setting root password mysql"
 echo show databases | mysql -uroot -p${mysql_root_password}  &>>${log_file}
 if [ $? -ne 0 ]; then
-print_header "setting root password mysql"
 mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log_file}
-status_check $?
 fi
 status_check $?
 
