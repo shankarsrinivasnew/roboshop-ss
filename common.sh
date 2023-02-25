87a1fc8ed5f88caa91292d7c35f31d7f0a96bcf4
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-25 21:06:16 +0530

    sdfg

--- common.sh
+++ common.sh
@@ -14,3 +14,67 @@ else
     exit 1
 fi
 }
+
+nodejs () {
+
+print_header "downloading code"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
+
+print_header "installing package"
+yum install nodejs -y &>>${log_file}
+status_check $?
+
+print_header "adding user"
+id roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
+useradd roboshop &>>${log_file}
+fi
+status_check $?
+
+print_header "adding new directory"
+if [ ! -d /app ]; then
+mkdir /app &>>${log_file}
+fi
+status_check $?
+
+print_header "removing old content"
+rm -rvf /app/* &>>${log_file}
+status_check $?
+
+print_header "downloading new code"
+curl -L -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip  &>>${log_file}
+status_check $?
+
+print_header "creating new directory"
+cd /app
+status_check $?
+
+print_header "unzipping new code"
+unzip /tmp/$component.zip &>>${log_file}
+cd /app
+status_check $?
+
+print_header "installing new code"
+npm install &>>${log_file}
+status_check $?
+
+print_header "copying old code"
+cp ${code_dir}/configs/$component.service /etc/systemd/system/$component.service &>>${log_file}
+status_check $?
+
+print_header "restarting service"
+systemctl daemon-reload &>>${log_file}
+systemctl enable $component &>>${log_file}
+systemctl restart $component &>>${log_file}
+status_check $?
+
+print_header "loading mogo repositories"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
+
+print_header "loading schema"
+mongo --host mongodb.sstech.store </app/schema/$component.js &>>${log_file}
+status_check $?
+}
--- user.sh
+++ user.sh
@@ -1,60 +1,6 @@
 source common.sh src
-
-print_header "downloading nodejs"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-status_check $?
-
-print_header "Installing nodejs"
-yum install nodejs -y &>>${log_file}
-status_check $?
-
-print_header "adding user"
-id roboshop &>>${log_file}
-if [ $? -ne 0 ]; then
-useradd roboshop &>>${log_file}
-fi
-status_check $?
-
-print_header "adding new directory"
-if [ ! -d /app ]; then
-mkdir /app &>>${log_file}
-fi
-status_check $?
-
-print_header "downlading new code and unzip files"
-rm -rvf /app/* &>>${log_file}
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${log_file}
-cd /app &>>${log_file}
-unzip /tmp/user.zip &>>${log_file}
-status_check $?
-
-print_header "downloading dependencies"
-cd /app &>>${log_file}
-npm install  &>>${log_file}
-status_check $?
-
-print_header "copyig systemd files"
-cp ${code_dir}/configs/user.service /etc/systemd/system/user.service  &>>${log_file}
-status_check $?
-
-print_header "starting user service"
-systemctl daemon-reload  &>>${log_file}
-systemctl enable user   &>>${log_file}
-systemctl start user  &>>${log_file}
-status_check $?
-
-print_header "loading yum repositories"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-status_check $?
-
-print_header "installing mongodb"
-yum install mongodb-org-shell -y &>>${log_file}
-status_check $?
-
-print_header "installing schema"
-mongo --host mongodb.sstech.store </app/schema/user.js &>>${log_file}
-status_check $?
-
+component=user
+nodejs
 
 
 
