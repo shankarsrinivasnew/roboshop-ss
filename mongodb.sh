977fbe7d8656f73ac7699f938171d1260a0522de
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-26 12:49:10 +0530

    dsfg

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 source common.sh
 component=mongodb
 
-print_head "Setup MongoDB repository"
+print_header "Setup MongoDB repository"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 status_check $?
 
@@ -9,14 +9,14 @@ print_header "installing mongodb"
 yum install mongodb-org -y &>>${log_file}
 status_check $?
 
-print_head "Update MongoDB Listen address"
+print_header "Update MongoDB Listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 status_check $?
 
-print_head "Enable MongoDB"
+print_header "Enable MongoDB"
 systemctl enable mongod &>>${log_file}
 status_check $?
 
-print_head "Start MongoDB Service"
+print_header "Start MongoDB Service"
 systemctl restart mongod &>>${log_file}
 status_check $?
